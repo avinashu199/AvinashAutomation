6a96318b71dcdf72a3307f8c54e11dc32f1538e0
avinashu199 <avinashu199@gmail.com>
2020-02-22 13:40:57 +0530

    demo commit

---
+++ src/test/java/Mavenframework/Sample.java
@@ -0,0 +1,15 @@
+package Mavenframework;
+
+import org.apache.logging.log4j.*;
+
+public class Sample {
+
+	private static Logger Log=LogManager.getLogger(Sample.class.getName());	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}

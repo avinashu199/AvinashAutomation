d325c14b89ef168347df2237c249fd695210e214
avinashu199 <avinashu199@gmail.com>
2020-02-22 17:53:11 +0530

    final script for 4.1.10

--- src/test/java/Mavenframework/Sample.java
+++ src/test/java/Mavenframework/Sample.java
@@ -1,14 +1,14 @@
 package Mavenframework;
 
-import org.apache.logging.log4j.*;
+
 
 public class Sample {
 
-	private static Logger Log=LogManager.getLogger(Sample.class.getName());	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		System.out.println("This is an updated program when compared to the older version");
 
 	}
 

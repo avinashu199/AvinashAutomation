ad6c24ee08f9273127647786fff065c9aee1b7fb
avinashu199 <avinashu199@gmail.com>
2020-02-22 18:21:59 +0530

    Cucumber in progress

--- src/test/java/Mavenframework/Sample.java
+++ src/test/java/Mavenframework/Sample.java
@@ -8,7 +8,7 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("This is an updated program when compared to the older version");
+		System.out.println("This is a cucumber framwork in building process");
 
 	}
 
